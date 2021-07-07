502513b39391cafe82ac4e3eeeaaea362d595c4b
ronnyvotel <25642953+ronnyvotel@users.noreply.github.com>
2021-07-07 09:09:53 -0700

    Adding FinanceCalculator to manage all expense calculations.

--- src/mortgage.js
+++ src/mortgage.js
@@ -3,9 +3,8 @@
  * @param  {Number} loan_amount The initial amount of the loan in dollars.
  * @param  {Number} int_rate    The interest rate as a decimal (e.g. 0.03).
  * @param  {Number} term_years  The number of years over which the mortgage is
- *                              acive (e.g. 30)
- * @return {Number}             The monthly payment, which includes principal
- *                              and interst (P&I). 
+ *  acive (e.g. 30)
+ * @return {Number} The monthly payment, which includes principal and interst. 
  */
 function monthly_mortgage(loan_amount, int_rate, term_years) {
   if (loan_amount < 0) {
@@ -25,10 +24,15 @@ function monthly_mortgage(loan_amount, int_rate, term_years) {
   return loan_amount * multiplier / total_sum;
 }
 
-/** Mortgatge. 
+/** Mortgage. 
  *  Class for computing mortgages.
  */
 class Mortgage {
+  /**
+   * @param {Number} loan_amount The amount of the loan ($).
+   * @param {Number} int_rate The interest rate.
+   * @param {Number} term_years The term of the loan in years.
+   */
   constructor(loan_amount, int_rate, term_years) {
     this._loan_amount = loan_amount;
     this._int_rate = int_rate;
@@ -42,6 +46,7 @@ class Mortgage {
 
   set loan_amount(amount) {
     if (amount !== this._loan_amount) {
+      this._loan_amount = amount;
       this.calculate();
     }
   }
@@ -50,6 +55,7 @@ class Mortgage {
 
   set int_rate(rate) {
     if (rate !== this._int_rate) {
+      this._int_rate = rate;
       this.calculate();
     }
   }
@@ -58,6 +64,7 @@ class Mortgage {
 
   set term_years(term) {
     if (term !== this._term_years) {
+      this._term_years = term;
       this.calculate();
     }
   }
@@ -66,4 +73,102 @@ class Mortgage {
     this._monthly_mortgage = monthly_mortgage(
       this._loan_amount, this._int_rate, this._term_years);
   }
+}
+
+/** FinanceCalculator.
+ * Class for computing all the finances.
+ */
+class FinanceCalculator {
+  /**
+   * @param {Number} price House price.
+   * @param {Number} percent_down The percent down on the home.
+   * @param {Number} int_rate The interest rate.
+   * @param {Number} term_years The term of the loan in years.
+   * @param {Number} income Yearly income before tax ($).
+   * @param {Number} insurance Yearly insurance bill ($).
+   * @param {Number} tax_rate The tax rate.
+   */
+  constructor(price = 1e6, percent_down = 0.20, int_rate = 0.032,
+    term_years = 30, income = 1e5, insurance = 1e3, tax_rate = 0.01) {
+    this._price = price;
+    this._percent_down = percent_down;
+    this._mortgage = new Mortgage(this.loan_amount, int_rate, term_years);
+    this._income = income;
+    this._insurance = insurance;
+    this._tax_rate = tax_rate;
+  }
+
+  get price() {return this._price;}
+
+  set price(value) {
+    if (value !== this._price) {
+      this._price = value;
+      this._mortgage.loan_amount = this.loan_amount;
+    }
+  }
+
+  get percent_down() {return this._percent_down;}
+
+  set percent_down(value) {
+    if (value !== this._percent_down) {
+      this._percent_down = value;
+      this._mortgage.loan_amount = this.loan_amount;
+    }
+  }
+
+  get int_rate() {return this._mortgage.int_rate;}
+
+  set int_rate(value) {
+    if (value !== this.int_rate) {
+      this._mortgage.int_rate = value;
+    }
+  }
+
+  get term_years() {return this._mortgage.term_years;}
+
+  set term_years(value) {
+    if (value !== this.term_years) {
+      this._mortgage.term_years = value;
+    }
+  }
+
+  get income() {return this._income;}
+
+  set income(value) {
+    this._income = value;
+  }
+
+  get insurance() {return this._insurance;}
+
+  set insurance(value) {
+    this._insurance = value;
+  }
+
+  get tax_rate() {return this._tax_rate;}
+
+  set tax_rate(value) {
+    this._tax_rate = value;
+  }
+
+  get loan_amount() {
+    return this.price * (1 - this.percent_down);
+  }
+
+  monthly_payment() {
+    return this._mortgage.monthly_payment;
+  }
+
+  monthly_tax() {
+    return this.price * this.tax_rate / 12;
+  }
+
+  monthly_insurance() {
+    return this.insurance / 12;
+  }
+
+  piti() {
+    return (this.monthly_payment() + this.monthly_tax() + 
+            this.monthly_insurance());
+  }
+
 }
